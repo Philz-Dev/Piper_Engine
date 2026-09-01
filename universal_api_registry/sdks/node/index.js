18ce6a6313b3b19122546e9f89fdb68ac4483aef
Philz-Dev <aliceemakpose@gmail.com>
2026-09-01 23:36:34 +0100

    corrected the folder tree

--- universal_api_registry/sdks/node/index.js
+++
@@ -1,8 +0,0 @@
-const path = require("path");
-
-/** Absolute path to the installed schemas/ tree. */
-function schemasRoot() {
-  return path.join(__dirname, "schemas");
-}
-
-module.exports = { schemasRoot };
